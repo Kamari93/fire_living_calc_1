8dcc9b58cb3a3d23387403e969d64eebeff3831e
Kamari <kmoore971@gmail.com>
2025-12-17 09:37:10 -0600

    Updated auth middleware to properly find user id

--- server/routes/user.js
+++ server/routes/user.js
@@ -11,7 +11,11 @@ function auth(req, res, next) {
   const token = authHeader.split(" ")[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
+    // req.user = decoded;
+    req.user = {
+      _id: decoded._id || decoded.id || decoded.userId,
+    };
+
     next();
   } catch {
     return res.status(401).json({ message: "Invalid token" });
