4eed064215a42ddf9bbb04666bb13c494bbf47c7
Kamari <kmoore971@gmail.com>
2025-12-29 12:41:47 -0600

    G: Updated file structure for proper build by moving api folder into src

--- client/src/pages/ProfileSettings.jsx
+++ client/src/pages/ProfileSettings.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 // import axios from "axios";
-import api from "client/api/axios";
+import api from "../api/axios";
 
 export default function ProfileSettings() {
   const [user, setUser] = useState(null);
