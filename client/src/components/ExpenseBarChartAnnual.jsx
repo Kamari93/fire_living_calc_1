7cec8e4eeea9b57b8168643fb05543afe0526c63
Kamari <kmoore971@gmail.com>
2026-01-29 14:12:56 -0600

    update chart container on comparison

--- client/src/components/ExpenseBarChartAnnual.jsx
+++ client/src/components/ExpenseBarChartAnnual.jsx
@@ -104,7 +104,7 @@ export default function ExpenseBarChartAnnual({ expenses, compact = false }) {
           )}
 
           <Tooltip formatter={currencyFormatter} />
-          <Bar dataKey="value" fill="#0ea5e9" barSize={28} maxBarSize={32} />
+          <Bar dataKey="value" fill="#0ea5e9" />
         </BarChart>
         {/* <BarChart
           data={data}
